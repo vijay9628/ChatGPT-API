357cc020a58d18a2d9ef7bc48618711d0ba38c77
adminn <adminn@DESKTOP-C1GJNQI>
2023-06-19 14:36:46 +0530

    Add project files.

---
+++ CHatGPTaPI/Controllers/OPENAIController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CHatGPTaPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class OPENAIController : ControllerBase
+    {
+        [HttpPost]
+        [Route("chatgpt")]
+         public IActionResult GetResult(string data)
+        {
+            string apikey = "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";
+            string resultdata = string.Empty;
+            var openAI = new OpenAIAPI(apikey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = data;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            completion.MaxTokens = 4000;
+            var result = openAI.Completions.CreateCompletionsAsync(completion);
+            if(result != null)
+            {
+                foreach(var item in result.Result.Completions)
+                {
+                    resultdata = item.Text;
+                }
+                return Ok(resultdata);
+            }
+            else
+            {
+                return BadRequest("not found");
+            }
+
+         }
+
+    }
+}
